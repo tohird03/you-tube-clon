aef09a7507ca3064c4d6b8caec108bea6d1557d2
tohird03 <tohirjondoniyorov@gmail.com>
2022-04-18 16:59:57 +0500

    finsih

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "../Explore/Explore.scss"
+import notVideo from "../../Assets/img/not.png"
 import trend from "../../Assets/img/trend1.png"
 import muzic from "../../Assets/img/music-dark.png"
 import live from "../../Assets/img/trend2.png"
@@ -13,6 +15,22 @@ const Explore = () => {
     const handleClick = (e) => {
         console.log(e.target.textContent);
     }
+
+    const [video, setVideo] = useState([])
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    //         'X-RapidAPI-Key': '7306d73337msh7fad6a0fa751d98p10c355jsna8f006a0cd81'
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     fetch('https://youtube-search-and-download.p.rapidapi.com/trending?type=C%20g%2C%20n%2C%20mo&hl=en&gl=US', options)
+    //         .then(response => response.json())
+    //         .then(response => setVideo(response))
+    //         .catch(err => console.error(err));
+    // }, []);
 
     return (
         <div className='explore'>
@@ -49,6 +67,45 @@ const Explore = () => {
                     <img className="explore__card-img" src={fashion} alt="" width="32" height="32" />
                     <h3 className='explore__card-heading'>Fashion & Beauty</h3>
                 </div>
+            </div>
+
+            <div className="videos">
+
+                {
+                    video?.contents?.map(i => {
+                        return <Link key={Math.random()} to={`/${i.video.channelId}/${i.video.videoId}/${i.video.title}/${i.video.viewCountText}/${i.video.publishedTimeText}`}>
+                            <div className="video">
+                                <div className="thumbnail">
+                                    <a href="https://www.youtube.com/">
+                                        <img src={i.video.thumbnails.map(i => {
+                                            return i.url
+                                        }) || notVideo} alt="" />
+                                    </a>
+                                </div>
+
+                                <div className="details">
+                                    <div className="author">
+                                        <img src={i.video.thumbnails[0].url}
+                                            alt="" />
+                                    </div>
+                                    <div className="title">
+                                        <h3>
+                                            {i.video.title}
+                                        </h3>
+                                        <a href="">
+                                            {i.video.channelName}
+                                        </a>
+                                        <span> {i.video.viewCountText} • {i.video.publishedTimeText} </span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Link>
+
+                    })
+                }
+
+                {/* https://www.youtube.com/watch?v=zpNZ6GxLlDI */}
             </div>
         </div>
     );
